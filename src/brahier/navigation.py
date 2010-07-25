0c7537cffee6481f7a715f3bc57c9f8fef575bee
Jacques Supcik <supcik@morpheus.(none)>
2010-07-25 12:34:46 +0200

    wip

--- src/brahier/navigation.py
+++ src/brahier/navigation.py
@@ -2,20 +2,12 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
 
 NAVIGATION = [
-    ['home', _('Home RA Portal'), '/', {'top' : 1}],
-    ['reg', _('Registration'), None, {}, [
-        ['reg_whois', _('Whois'), {}, '/'],
-        ['reg_search_cert', _('Search Certificate'), {}, '/'],
-        ['reg_decode_req', _('Decode/Check CSR'), {}, '/'],
-        ['reg_decode_cert', _('Decode Cert'), {}, '/'],
+    ['home', _('Accueil'), '/', {'top' : 1}],
+    ['webcam', _('Webcam'), '/webcam', {}],
+
+    ['concours', _('Concours'), '/concours', {}, [
+        ['c_2010', _('2010'), '/concours/2010', {}],
+        ['c_2009', _('2009'), '/concours/2009', {}],
     ]],
-    ['doc', _('Documents'), None, {}, [
-        ['doc_manual', _('Documents/Manuals'), '/', {}],
-        ['doc_faq', _('F.A.Q.'), '/', {}],
-        ['doc_news', _('News'), '/', {}],
-    ]],
-    ['fmh', _('FMH'), None, {}, [
-        ['fmh_revoke', _('Revoke'), '/', {}],
-    ]],
-    ['admin', _('Admin'), reverse('admin:index'), {}],
+#    ['admin', _('Admin'), reverse('admin:index'), {}],
 ]
