e897f5b2b5c31f980844949b7749c90a239c1261
supcik <supcik@morpheus>
2010-07-26 00:12:50 +0200

    bug fix

--- src/brahier/lesson/models.py
+++ src/brahier/lesson/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from brahier.horse.models import import Horse
+from brahier.horse.models import Horse
 
 class Lesson(models.Model):
     name = models.CharField(max_length=255)
     day = models.PositiveSmallIntegerField()
     time = models.TimeField()
     duration = models.IntegerField()
-    type = models.CharField(max_lenght=255, blank=True)
+    type = models.CharField(max_length=255, blank=True)
     max_places = models.IntegerField()
 
 class Event(models.Model):
