de30216b8ba1a349607b36f97c44b03b6d1056f8
Jacques Supcik <jacques@supcik.net>
2010-08-02 01:13:28 +0200

    wip

--- src/brahier/account/models.py
+++ src/brahier/account/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
-    date_of_birth = models.DateField(blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     address = models.CharField(max_length=255, blank=True)
     zip_code = models.CharField(max_length=32, blank=True)
     location = models.CharField(max_length=255, blank=True)
     phone_number = models.CharField(max_length=255, blank=True)
     mobile_number = models.CharField(max_length=255, blank=True)
+    registration_challenge = models.CharField(max_length=100, blank=True)
--- src/brahier/account/views.py
+++ src/brahier/account/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.template import RequestContext
+
+from brahier.account.forms import RegistrationForm
 
 def profile(request):
     return render_to_response("registration/profile.html", None,
         context_instance=RequestContext(request))
+    
+def new(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = RegistrationForm()
+
+    return render_to_response('registration/new.html', {
+            'form': form,
+        },
+        context_instance=RequestContext(request)
+    )
